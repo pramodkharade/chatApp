a9c3723f1bc2d98cc495b91b9fd101ceb2527db3
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-25 16:07:39 +0530

    set call default public template call

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ const express =  require('express');
 const path = require('path');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 publicDirectoryPath = path.join(__dirname,'../public');
+
 app.use(express.static(publicDirectoryPath));
 app.listen(port,()=>{
-    console.log("Server is running on ", port);
+    console.log(`Server is running on ${port}`);
 });
