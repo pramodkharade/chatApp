8915964e388d8e03c8f8b702c60b377dea75326d
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-25 15:53:04 +0530

    config the public directory with express object

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 publicDirectoryPath = path.join(__dirname,'../public');
-
+app.use(express.static(publicDirectoryPath));
 app.listen(port,()=>{
     console.log("Server is running on ", port);
 });
