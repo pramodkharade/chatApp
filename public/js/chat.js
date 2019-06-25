dbd6c90adc0837441d4cd95420aca1c347b58c9f
pramodkharade <kharade.pramod91@gmail.com>
2019-06-25 05:30:45 +0530

    share the current location cords and google map link

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,3 +9,16 @@ document.querySelector('#message-form').addEventListener('submit',(e)=>{
 socket.on('message',(msg)=>{
     console.log(msg);
 });
+
+document.querySelector('#send-location').addEventListener('click',(e)=>{
+    if(!navigator.geolocation){
+        return alert('Geolocation is not supportted to your browser.');
+    }
+    navigator.geolocation.getCurrentPosition((position)=>{
+        socket.emit('sendLocation',{
+            latitude:position.coords.latitude,
+            longitude:position.coords.longitude
+        });
+        
+    });
+});
