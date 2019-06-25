524b82427de6503baa2ed2a35539162d26383121
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-25 17:44:37 +0530

    configure the websocket

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express =  require('express');
 const path = require('path');
+const http = require('http');
 const app = express();
+const server = http.createServer(app);
 
 const port = process.env.PORT || 4000;
 publicDirectoryPath = path.join(__dirname,'../public');
 
 app.use(express.static(publicDirectoryPath));
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Server is running on ${port}`);
 });
