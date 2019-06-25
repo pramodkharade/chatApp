83694b6f9b641c77f46000644a8ed93b0b633925
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-25 15:46:05 +0530

    public directory using path library

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express =  require('express');
+const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 3000;
+publicDirectoryPath = path.join(__dirname,'../public');
 
 app.listen(port,()=>{
     console.log("Server is running on ", port);
